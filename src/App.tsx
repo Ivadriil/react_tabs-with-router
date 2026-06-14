c39e9a9d5b8235326b6b548c7bfb0e6fceae7e2d
Ivadriil <petrivi51@gmail.com>
2026-06-14 11:24:03 +0300

    plus Location

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
-          <Link to="/tabs" className="navbar-item">
-            Tabs
-          </Link>
+export const App = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/"
+              className={location.pathname === '/' ? 'is-active' : ''}
+            >
+              Home
+            </Link>
+            <Link
+              to="/tabs"
+              className={
+                location.pathname.startsWith('/tabs') ? 'is-active' : ''
+              }
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Outlet />
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Outlet />
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
